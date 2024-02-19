bfd38a264e605308643d12705ad7605d9c6d9fd7
mustafakızılay <mustafakzly51@gmail.com>
2024-02-19 16:04:52 +0300

    Sepet kısmı yapıldı

--- alisveris/src/components/navbar/nav-center.js
+++ alisveris/src/components/navbar/nav-center.js
@@ -1,38 +1,63 @@
-import "../../styles/navbar/nav-center.css"
-import { NavLink } from 'react-router-dom';
-import Logo from '../../assets/logo.png';
-import Search from '../../assets/search.png';
-import User from '../../assets/user.png';
-import Basket from '../../assets/shopping-basket.png';
+import React, { useState } from "react";
+import "../../styles/navbar/nav-center.css";
+import { NavLink } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+import Search from "../../assets/search.png";
+import User from "../../assets/user.png";
+import Basket from "../../assets/shopping-basket.png";
+import Close from "../../assets/close.png";
+
 function Navcenter() {
-    return ( 
-        <div className="navbar-center">
-            <div className="navbar-center-left">
-                <NavLink to = '/'>
-                    <img src={Logo} alt=""/>
-                </NavLink>
-            </div>
-            <div className="navbar-center-right">
-                <div className="search">
-                    <input type="search" placeholder="Ne aramıştınız ?"/> 
-                    <img src={Search} alt=""></img>
-                </div>
-                <div className="account">
-                    <NavLink to='/login'>
-                        <img src={User} alt=""/>
-                    </NavLink>
-                </div>
-                <div className="basket">
-                <NavLink to='/'>
-                        <img src={Basket} alt=""/>
-                    </NavLink>
-                </div>
-            </div>
-            <div className="number">
-                <p>0</p>
-            </div>
+  const [isBasketOpen, setIsBasketOpen] = useState(false);
+  const toggleBasket = () => setIsBasketOpen(!isBasketOpen);
+
+  return (
+    <div className="navbar-center">
+      <div className="navbar-center-left">
+        <NavLink to="/">
+          <img src={Logo} alt="" />
+        </NavLink>
+      </div>
+      <div className="navbar-center-right">
+        <div className="search">
+          <input type="search" placeholder="Ne aramıştınız ?" />
+          <img src={Search} alt="" />
         </div>
-     );
+        <div className="account">
+          <NavLink to="/login">
+            <img src={User} alt="" />
+          </NavLink>
+        </div>
+        <div className="basket">
+          <button onClick={toggleBasket}>
+            <img src={Basket} alt="" className="basket-image"/>
+          </button>
+          {isBasketOpen && ( 
+            <div className="basket-menu">
+              <button className="basket-close-btn" onClick={toggleBasket}>
+                <img src={Close} alt="Close" />
+              </button>
+              <div className="basket-menu-position">
+                <div className="basket-menu-img">
+                  <img
+                    src="https://ideacdn.net/idea/ed/42/myassets/products/064/yuzuk-3_min.png?revision=1697143329"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <p>Ürün Adı dsadsadsadsadsadsasdadsadsa</p>
+                  <p>Ürün Fiyatı</p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="number">
+        <p>0</p>
+      </div>
+    </div>
+  );
 }
 
 export default Navcenter;
